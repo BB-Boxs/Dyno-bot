b060312ab5523373f25535cc69ebe61dffa0f8e9
BB-Boxs <31480265+BB-Boxs@users.noreply.github.com>
2018-02-27 18:17:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ var fortunes = [
 bot.on("ready", function() {
 	console.log("ready");
 	
-	bot.user.setStatus("invisible");
+	bot.user.setStatus("dnd");
 	bot.user.setGame("dynobot.net|?help");
 });
 
